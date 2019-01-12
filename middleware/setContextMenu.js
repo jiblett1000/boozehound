c82276a832fa9e4ad25ee3ad4b927b825c034dc0
Christian Burkhart <worm@christianburkhart.com>
2019-01-12 02:50:49 -0800

    misc

--- middleware/setContextMenu.js
+++ middleware/setContextMenu.js
@@ -1,4 +1,5 @@
 export default function ({store, route}) {
   const contextMenu = route.meta.reduce((contextMenu, meta) => meta.contextMenu, 0)
+  
   store.dispatch('contextMenu/set', contextMenu);
 }
