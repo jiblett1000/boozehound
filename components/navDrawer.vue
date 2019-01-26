59e086942c4f920ded383107e77ee03c0930f9e2
Christian Burkhart <worm@christianburkhart.com>
2019-01-25 23:08:36 -0800

    move main menu list items from store to componenet data

--- components/navDrawer.vue
+++ components/navDrawer.vue
@@ -59,9 +59,57 @@
 import { mapState } from 'vuex';
 
 export default {
+  data() {
+    return {
+      mainMenu:[
+        { 
+          href: '/dashboard', 
+          title: 'Dashboard', 
+          icon: 'dashboard', 
+        },
+        { 
+          href: '/recipes', 
+          title: 'Recipes', 
+          icon: 'book', 
+        },
+        { 
+          href: '/inventory', 
+          title: 'Inventory', 
+          icon: 'ballot', 
+        },
+        { 
+          href: '/orders',
+          title: 'Orders', 
+          icon: 'view_list', 
+        },
+        { 
+          href: '/pos-items',
+          title: 'POS Items', 
+          icon: 'tablet_android', 
+        },
+        { 
+          href: '/reports', 
+          title: 'Reports', 
+          icon: 'trending_up', 
+        },
+        { 
+          divider: true,
+        },
+        { 
+          href: '/settings',
+          title: 'Settings', 
+          icon: 'settings', 
+        },
+        { 
+          href: '/help',
+          title: 'Help', 
+          icon: 'help', 
+        },
+      ],
+    }
+  },
   computed: {
     ...mapState(['fullName']),
-    ...mapState('mainMenu', ['mainMenu']),
     
     drawer: {
       get() {
